699d71ae62b548b0698cedbe34c1583b672fbf0b
Rainer Sigwald <raines@microsoft.com>
2021-04-30 12:29:37 -0500

    Command line handling

--- PerformanceSummaryToCsv/Program.cs
+++ PerformanceSummaryToCsv/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PerformanceSummaryToCsv
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand
+            {
+                new Option<FileInfo[]>(
+                    "--inputs",
+                    "Input text files containing an MSBuild PerformanceSummary section",
+                    ArgumentArity.OneOrMore),
+                new Option<FileInfo>(
+                    "--output",
+                    getDefaultValue: () => new FileInfo("MSBuild_performance.csv"),
+                    "Path of the final csv file.")
+            };
+
+            rootCommand.Description = "Converts and aggregates MSBuild text performance summaries into a CSV.";
+
+            // Note that the parameters of the handler method are matched according to the names of the options
+            rootCommand.Handler = CommandHandler.Create<FileInfo[], FileInfo>(Aggregate);
+
+            // Parse the incoming args and invoke the handler
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        async static Task Aggregate(FileInfo[] inputs, FileInfo output)
+        {
+            Console.WriteLine($"Aggregating {string.Join(", ", inputs.Select(fi => fi.FullName))}");
         }
     }
 }
